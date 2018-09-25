dc5d4d08be54f15a98710ac9096a009c2f24f296
Daniel Friedenbach <danielfriedenbach@gmail.com>
2018-09-25 16:43:27 -0700

    implement penetration dice and honor bonus/penalty

--- src/dice.ts
+++ src/dice.ts
@@ -1,12 +1,40 @@
 import { randInt } from './util';
 
-export function rollDie(size: number, modifier: number = 0): number {
-  return randInt(1, size) + modifier;
+export enum Honor {
+  Poor = -1,
+  Average = 0,
+  Great = 1,
+}
+
+export interface IRollOptions {
+  penetration?: boolean;
+  crossbow?: boolean;
+  honor?: Honor,
+}
+const defaultOptions = Object.freeze({
+  honor: Honor.Average,
+});
+
+function rollDieInternal(size: number): number {
+  return randInt(1, size);
+}
+
+export function rollDie(size: number, modifier: number = 0, options: IRollOptions = defaultOptions): number {
+  let result = modifier;
+  let rolled = rollDieInternal(size);
+  result += rolled + (options.honor || Honor.Average);
+  if (options.penetration && size > 2) {
+    while (rolled === size || options.crossbow && rolled === size - 1) {
+      rolled = rollDieInternal(size);
+      result += rolled - 1 + (options.honor || Honor.Average);
+    }
+  }
+  return result;
 }
 
 const diceRegex = /(\d*)d(\d+)([+-]\d+)?/i;
 
-export function roll(input: string): number | undefined {
+export function roll(input: string, options: IRollOptions = defaultOptions): number | undefined {
   const match = diceRegex.exec(input);
   if (!match) {
     return;
@@ -17,7 +45,7 @@ export function roll(input: string): number | undefined {
 
   let result = 0;
   for (let i = 0; i < quantity; i++) {
-    result += rollDie(die);
+    result += rollDie(die, 0, options);
   }
   return result + modifier;
 }
