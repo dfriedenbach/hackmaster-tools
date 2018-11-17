49085fa1ee173c1a74db17d15ef090fda61c235b
Daniel Friedenbach <danielfriedenbach@gmail.com>
2018-11-16 16:29:46 -0800

    keep index.tsx in src/ to satisfy react-scripts-ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import App from 'client/App';
+import 'client/index.css';
+import registerServiceWorker from 'client/registerServiceWorker';
 
 ReactDOM.render((
     <Router>
